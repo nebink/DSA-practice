008829adb526cca66ce73bd0524f3cd5fff5b7f6
Nebin K Raj <96125944+nebink@users.noreply.github.com>
2023-09-22 23:43:15 +0530

    Add files via upload

---
+++ Minimize the maximum difference between the heights.py
@@ -0,0 +1,30 @@
+
+def getMinDiff(arr, n, k):
+	arr.sort()
+	ans = arr[n - 1] - arr[0] 
+
+	tempmin = arr[0]
+	tempmax = arr[n - 1]
+
+	for i in range(1, n):
+		if arr[i] < k:
+			continue
+		tempmin = min(arr[0] + k, arr[i] - k)
+
+		# Minimum element when we
+		# add k to whole array
+		# Maximum element when we
+		tempmax = max(arr[i - 1] + k, arr[n - 1] - k)
+
+		# subtract k from whole array
+		ans = min(ans, tempmax - tempmin)
+
+	return ans
+
+
+
+k = 6
+n = 6
+arr = [7, 4, 8, 8, 8, 9]
+ans = getMinDiff(arr, n, k)
+print(ans)
